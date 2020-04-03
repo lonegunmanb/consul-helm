78baa1b817760756e6c034fbcb23b68d8f602cc8
lonegunman <lonegunmanb@hotmail.com>
2020-04-03 16:18:42 +0800

    add server pod request variables to variables.tf, add consul ui ingress

--- variables.tf
+++ variables.tf
@@ -1,20 +1,45 @@
 variable namespace {
-  type    = "string"
+  type    = string
   default = "default"
 }
 variable "image" {
-  type = "string"
+  type = string
   default = "consul:1.7.1"
 }
 variable "storageClass" {
-  type = "string"
+  type = string
   default = ""
 }
-variable "url_tail" {
-  type = "string"
-  default = ".$${NAMESPACE}.svc"
-}
 variable "storage" {
-  type = "string"
+  type = string
   default = "20Gi"
+}
+
+variable "enable_public_ui" {
+  type = bool
+  default = false
+}
+variable "server_request_memory" {
+  type = string
+  default = "128Mi"
+}
+
+variable "server_request_cpu" {
+  type = string
+  default = "250m"
+}
+
+variable "server_limit_memory" {
+  type = string
+  default = "256Mi"
+}
+
+variable "server_limit_cpu" {
+  type = string
+  default = "500m"
+}
+
+variable "url_tail" {
+  type = string
+  default = ".$${NAMESPACE}.svc"
 }
